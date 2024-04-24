cb14a5d89cebf1990df055c1400dd4f62533fd17
LindseyyyW <152750390+LindseyyyW@users.noreply.github.com>
2024-04-23 17:23:43 -0700

    Update scenario1.py with topk, filter, and aggregation

--- GUI/backend/scenario1.py
+++ GUI/backend/scenario1.py
@@ -8,10 +8,15 @@ CORS(app)
 def topk_search():
     data = request.json
     k = data.get('k')
+    k = int(k)
     roi = 'True' if data.get('roi') == 'object bounding box' else 'False'
+    region = 'object' if roi == 'True' else 'custom'
     pixel_upper_bound = data.get('pixelUpperBound')
     pixel_lower_bound = data.get('pixelLowerBound')
+    lv = float(pixel_lower_bound)
+    uv = float(pixel_upper_bound)
     order = data.get('order')
+    reverse = False if order == 'DESC' else True
     
     query_command = f"""
     SELECT mask_id
@@ -19,28 +24,112 @@ def topk_search():
     ORDER BY CP(mask, roi, ({pixel_lower_bound}, {pixel_upper_bound})) / area(roi) {order}
     LIMIT {k};
     """
-    
+    start = time.time()
     # Dummy implementation to return the query command and some mock image IDs
-    image_ids = list(range(1, int(k) + 1))
+    hist_size = 16
+    bin_width = 256 // hist_size
+    cam_size_y = 448
+    cam_size_x = 448
+    region_area_threshold = 5000
+    available_coords = 64
+    
+    ps = time.time()
+    print("data process: ", ps - start)
+    count, images = get_max_area_in_subregion_in_memory_version(
+    "wilds",
+    (id_val_data, ood_val_data),
+    label_map,
+    pred_map,
+    cam_map,
+    object_detection_map,
+    bin_width,
+    cam_size_y,
+    cam_size_x,
+    hist_size,
+    dataset_examples,
+    lv,
+    uv,
+    region,
+    in_memory_index_suffix,
+    image_access_order,
+    early_stoppable=False,
+    k=k,
+    region_area_threshold=region_area_threshold,
+    ignore_zero_area_region=True,
+    reverse=reverse,
+    visualize=True,
+    available_coords=available_coords,
+    compression=None,
+    )
+    
+    image_ids = [image_idx for (metric, area, image_idx) in images]
+    end = time.time()
+    print("time: ", end - start)
     return jsonify({'query_command': query_command, 'image_ids': image_ids})
+
+
+@app.route('/api/augment', methods=['POST'])
+def augment():
+    data = request.json
+    img_ids = data.get('image_ids')
+    # Now you can use img_ids for augmentation
+    print(img_ids)
+    return jsonify({'image_ids': img_ids})
+
+
 
 @app.route('/api/filter_search', methods=['POST'])
 def filter_search():
     data = request.json
     threshold = data.get('threshold')
+    v = float(threshold)
     roi = 'True' if data.get('roi') == 'object bounding box' else 'False'
+    region = 'object' if roi == 'True' else 'custom'
     pixel_upper_bound = data.get('pixelUpperBound')
     pixel_lower_bound = data.get('pixelLowerBound')
     comparison = data.get('thresholdDirection')
+    reverse = True if comparison == '<' else False
+    lv = float(pixel_lower_bound)
+    uv = float(pixel_upper_bound)
     
     query_command = f"""
     SELECT mask_id
     FROM MasksDatabaseView
     WHERE CP(mask, roi, ({pixel_lower_bound}, {pixel_upper_bound})) / area(roi) {comparison} {threshold};
     """
-    
+
+    hist_size = 16
+    bin_width = 256 // hist_size
+    cam_size_y = 448
+    cam_size_x = 448
+    region_area_threshold = 5000
+    available_coords = 64
+    count, images = get_images_satisfying_filter("wilds",
+        cam_map,
+        object_detection_map,
+        in_memory_index_suffix,
+        bin_width,
+        hist_size,
+        cam_size_y,
+        cam_size_x,
+        dataset_examples,
+        lv,
+        uv,
+        region,
+        v,
+        region_area_threshold,
+        True,
+        available_coords,
+    None,
+    reverse=reverse)
+    num = 0
+    if(len(images)>50): 
+        num = 50
+    else: 
+        num = len(images)
+    print("*", len(images), num)
+    image_ids = [image_idx for (metric,image_idx) in images[:num]]
     # Dummy implementation to return the query command and some mock image IDs
-    image_ids = list(range(1, 10))  # Mock 6 images for the filter results
     return jsonify({'query_command': query_command, 'image_ids': image_ids})
 
 @app.route('/api/aggregation_search', methods=['POST'])
@@ -73,6 +162,10 @@ def filter_image(filename):
 @app.route('/aggregation_results/<filename>')
 def aggregation_image(filename):
     return send_from_directory('aggregation_results', filename)
+
+@app.route('/augment_results/<filename>')
+def augment_image(filename):
+    return send_from_directory('augment_results', filename)
 
 if __name__ == '__main__':
     app.run(port=8000)
