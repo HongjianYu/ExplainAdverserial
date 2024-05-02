e0da4301b40ee51cf25d29cfc7c921cb091a2aa1
LindseyyyW <152750390+LindseyyyW@users.noreply.github.com>
2024-05-02 15:28:27 -0700

    Update scenario1.py

--- GUI/backend/scenario1.py
+++ GUI/backend/scenario1.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS
-
+from flask_cors import CORS, cross_origin
+import sys
+sys.path.append("/Users/lindseywei/MaskSearch/wilds")
+from topk import *
+from s1_data_process import data_process
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/api/topk_search', methods=['POST'])
 def topk_search():
@@ -132,25 +136,6 @@ def filter_search():
     # Dummy implementation to return the query command and some mock image IDs
     return jsonify({'query_command': query_command, 'image_ids': image_ids})
 
-@app.route('/api/aggregation_search', methods=['POST'])
-def aggregation_search():
-    data = request.json
-    threshold = data.get('threshold')
-    roi = 'True' if data.get('roi') == 'object bounding box' else 'False'
-    pixel_upper_bound = data.get('pixelUpperBound')
-    pixel_lower_bound = data.get('pixelLowerBound')
-    comparison = data.get('thresholdDirection')
-    
-    query_command = f"""
-    SELECT mask_id
-    FROM MasksDatabaseView
-    WHERE CP(mask, roi, ({pixel_lower_bound}, {pixel_upper_bound})) / area(roi) {comparison} {threshold};
-    """
-    
-    # Dummy implementation to return the query command and some mock image IDs
-    image_ids = list(range(1, 10))  # Mock 6 images for the filter results
-    return jsonify({'query_command': query_command, 'image_ids': image_ids})
-
 @app.route('/topk_results/<filename>')
 def topk_image(filename):
     return send_from_directory('topk_results', filename)
@@ -159,13 +144,11 @@ def topk_image(filename):
 def filter_image(filename):
     return send_from_directory('filter_results', filename)
 
-@app.route('/aggregation_results/<filename>')
-def aggregation_image(filename):
-    return send_from_directory('aggregation_results', filename)
-
 @app.route('/augment_results/<filename>')
 def augment_image(filename):
     return send_from_directory('augment_results', filename)
 
 if __name__ == '__main__':
+    #app.run(debug=True)
+    id_val_data, ood_val_data, label_map, pred_map, cam_map, object_detection_map, dataset_examples, in_memory_index_suffix, image_access_order = data_process()
     app.run(port=8000)
